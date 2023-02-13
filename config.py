9ed17308b8e06a50911e4a3c75b3b58ef6213662
Yamac <yamac@ceng.metu.edu.tr>
2023-02-13 06:34:12 +0300

    Add dot-env

--- config.py
+++ config.py
@@ -1,12 +1,15 @@
 from pydantic import BaseSettings
 
 
-class Config(BaseSettings):
+class Settings(BaseSettings):
     train_api_key: str = "very-secret-api-key"
     query_api_key: str = "very-secret-other-api-key"
     duplicate_threshold: int = 80
     upload_folder: str = 'data'
     allowed_extensions: list = ['csv']
 
+    class Config:
+        env_file = ".env"
 
-settings = Config()
+
+settings = Settings()
