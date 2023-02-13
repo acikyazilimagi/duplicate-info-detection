989346f5fc9bae72c0ab5f2b6923bd734ed0b2c7
Yamac <yamac@ceng.metu.edu.tr>
2023-02-13 03:52:13 +0300

    Add configs

---
+++ config.py
@@ -0,0 +1,12 @@
+from pydantic import BaseSettings
+
+
+class Config(BaseSettings):
+    train_api_key: str = "very-secret-api-key"
+    query_api_key: str = "very-secret-other-api-key"
+    duplicate_threshold: int = 80
+    upload_folder: str = 'data'
+    allowed_extensions: dict = {'csv'}
+
+
+settings = Config()
