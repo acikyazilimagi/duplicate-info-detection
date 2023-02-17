d1eecc54b8d84330c1cdc812118e877f79a2817e
savasalturk <alturksavas@gmail.com>
2023-02-17 03:05:54 +0300

    tfidf tek bir döngüye eklendi.

--- similarity.py
+++ similarity.py
@@ -86,20 +86,20 @@ def similarity_score(rows):
     return parsed
 
 
-def train_tfidf_from_csv(data_path="data/merged_v1.csv"):
-    # dff = pd.read_csv('data/merged_v1_5.csv')
-    dff = run_preprocess(dff)
-    isimler = dff["Ad-Soyad"].values
-    # train and store TF-IDF
+def train_tfidf_from_csv(data_path="data/merged_v1_4.csv"):
+    dff = pd.read_csv('data/merged_v1_4.csv')
+    dff1 = run_preprocess(dff)
+    # isimler = dff["Ad-Soyad"].values
+    # # train and store TF-IDF
 
-    # print("Training TF-IDF names model")
-    isim_vectorizer = TfidfVectorizer()
-    tfidff_isim = isim_vectorizer.fit(isimler)
-    tfidff_isim_vectors = isim_vectorizer.transform(isimler)
-    # print('tf idf isim done')
-    # print("Saving TF-IDF names model")
-    pkl.dump((tfidff_isim, tfidff_isim_vectors),
-             open("models/tfidff_isim.pkl", 'wb'))
+    # # print("Training TF-IDF names model")
+    # isim_vectorizer = TfidfVectorizer()
+    # tfidff_isim = isim_vectorizer.fit(isimler)
+    # tfidff_isim_vectors = isim_vectorizer.transform(isimler)
+    # # print('tf idf isim done')
+    # # print("Saving TF-IDF names model")
+    # pkl.dump((tfidff_isim, tfidff_isim_vectors),
+    #          open("models/tfidff_isim.pkl", 'wb'))
     replacement = {
         'sk': 'sokak',
         'sok': 'sokak',
@@ -114,11 +114,12 @@ def train_tfidf_from_csv(data_path="data/merged_v1.csv"):
     }
 
     # [(dff["oran_isim"] >0.90) ] #& (dfff["benzer_id_isim"] != dfff["id"])
-    dff1 = dff
+    
     dff1 = dff1.fillna("")
     dff1["text"] = dff1['Bina Adı'] + " " + dff1['Dış Kapı/ Blok/Apartman No'] \
         + " " + dff1["Bulvar/Cadde/Sokak/Yol/Yanyol"] + " " + dff1["new_adres"]
     text_tfidf_models = dict()
+    dff_concat = []
     for i in dff1.groupby(['İl', 'İlçe', 'Mahalle']):
         dff_i = i[1]
 
@@ -131,6 +132,7 @@ def train_tfidf_from_csv(data_path="data/merged_v1.csv"):
         dff_i["text"] = dff_i["text"].replace(replacement_sokak)
         dff_i["text"] = dff_i["text"].apply(text_edit)
         text = dff_i["text"]
+        isimler = dff_i["Ad-Soyad"]
         try:
             # print("Training TF-IDF text model for " + str(i[0]))
             text_vectorizer = TfidfVectorizer()
@@ -138,8 +140,63 @@ def train_tfidf_from_csv(data_path="data/merged_v1.csv"):
             vectors = text_vectorizer.transform(text)
             text_tfidf_models[str(i[0])] = (tfidff, vectors)
             # print('tf idf text done')
+
+            tfidff = TfidfVectorizer().fit_transform(text)
+                    
+            id_dff = dff_i["id"].values
+            pairwise_similarity = tfidff * tfidff.T
+            idler = []
+            oran = []
+            for idx in pairwise_similarity.toarray():
+                id_bulma = [ids for ids in idx]
+                try:
+                    idler.append(id_dff[id_bulma.index(sorted(id_bulma,reverse=True)[1])])
+                    oran.append(sorted(id_bulma,reverse=True)[1])
+                    
+                except:
+                    idler.append(None)
+                    oran.append(None)
+        
+            dff_i["benzer_id"] = idler
+            
+            dff_i["oran"] = oran
+            dff_i["İl"] = i[0][0]
+            dff_i["İlçe"] = i[0][1]
+            dff_i["Mahalle"] = i[0][2]
+
+
+            # Ad-Soyad TFIDF
+            id_dff_isim = id_dff
+            tfidff_isim = TfidfVectorizer().fit_transform(isimler)
+            pairwise_similarity_isim = tfidff_isim * tfidff_isim.T
+            idler = []
+            oran = []
+            for idx in pairwise_similarity_isim.toarray():
+                id_bulma = [ids for ids in idx]
+                try:
+                    idler.append(id_dff_isim[id_bulma.index(sorted(id_bulma,reverse=True)[1])])
+                    oran.append(sorted(id_bulma,reverse=True)[1])
+                except:
+                    idler.append(None)
+                    oran.append(None)
+
+            dff_i["benzer_id_isim"] = idler
+            dff_i["oran_isim"] = oran
+
+            dff_filtre = dff_i #[(dff_i["oran"] > 0.40) ] #& (dff_i["benzer_id"] != dff_i["id"])
+            if len(dff_filtre)>0:
+                dff_concat.append(dff_filtre)
         except ValueError:
             print("Passed model for " + str(i[0]))
 
     # print("Saving TF-IDF text indexed models")
+    dffc = pd.concat(dff_concat,ignore_index=True)
+    dffc.to_excel("deneme.xlsx",index=False) # csv yapılacak
     pkl.dump(text_tfidf_models, open("models/tfidff_text.pkl", 'wb'))
+
+
+
+
+
+if __name__ == "__main__":
+    train_tfidf_from_csv()
