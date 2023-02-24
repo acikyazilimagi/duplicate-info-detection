0928801573748d4a049484a95e48205197523018
Kerem Goksel <bkgoksel@microsoft.com>
2023-02-24 15:23:36 -0800

    clustering-csv-marking-works

--- name_address_clustering.py
+++ name_address_clustering.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import DBSCAN
+from sklearn.metrics import pairwise_distances
 import numpy as np
 import typer
 
@@ -27,33 +28,80 @@ def merge_address_columns(
 def cluster_by_column(
         df: pd.DataFrame,
         key_column_name: str,
-        cluster_column_name: str,
-        similarity_threshold: float = 0.1,
+        duplicate_max_distance_threshold: float = 0.1,
         tfidf_ngram_range: tuple = (1, 1),
+        tfidf_min_df: int = 1,
+        tfidf_use_char_ngrams: bool = False,
         df_mask = None,
 ) -> pd.DataFrame:
-    name_vectorizer = TfidfVectorizer(ngram_range=tfidf_ngram_range)
-    name_tfidf_vectors = name_vectorizer.fit_transform(df.loc[df_mask, key_column_name])
-    name_dbscan = DBSCAN(eps=similarity_threshold, min_samples=2, metric="cosine").fit(name_tfidf_vectors)
-    df.loc[df_mask, cluster_column_name] = name_dbscan.labels_
+    # index the rows that will be clustered
+    df.loc[df_mask, "clustering_index"] = list(range(df.loc[df_mask].shape[0]))
+
+    # derive names for cluster information columns
+    cluster_column_name = f"{key_column_name}-cluster"
+    duplicate_info_column_name = f"{key_column_name}-duplicate"
+    similarity_column_name = f"{key_column_name}-duplicate-similarity"
+    duplicate_original_column_name = f"{key_column_name}-duplicate-original-id"
+
+    analyzer = "char_wb" if tfidf_use_char_ngrams else "word"
+    vectorizer = TfidfVectorizer(analyzer=analyzer, ngram_range=tfidf_ngram_range, min_df=tfidf_min_df)
+    vectors = vectorizer.fit_transform(df.loc[df_mask, key_column_name])
+    # compute pairwise cosine distances between all rows
+    distance_matrix = pairwise_distances(vectors, vectors, metric="cosine")
+    # run the DBSCAN clustering algorithm using the pairwise distances
+    dbscan = DBSCAN(eps=duplicate_max_distance_threshold, min_samples=2, metric="precomputed") \
+        .fit(distance_matrix)
+    # annotate each row with the id of the cluster it belongs to
+    df.loc[df_mask, cluster_column_name] = dbscan.labels_
+
+    # process each cluster for marking reference points and similarity scores to the reference point
+    for cluster in np.unique(dbscan.labels_):
+        if cluster == "-1":
+            # -1 is the no-cluster cluster label, no need to do anything else
+            continue
+
+        # create a mask for the rows in the cluster
+        cluster_mask = df_mask & (df[cluster_column_name] == cluster)
+
+        # the first entry in the cluster is picked as the "original"
+        original_row_mask = df.index[cluster_mask][0]
+        duplicate_row_mask = df.index[cluster_mask][1:]
+        original_row = df.loc[original_row_mask]
+
+        # fetch the similarities of each row to the original row
+        original_row_similarities = 1.0 - distance_matrix[original_row["clustering_index"], :]
+
+        # mark the original row with "O"
+        df.loc[original_row_mask, duplicate_info_column_name] = "O"
+        # mark the other rows with "D"
+        df.loc[duplicate_row_mask, duplicate_info_column_name] = "D"
+        # mark every row with the original row's id
+        df.loc[cluster_mask, duplicate_original_column_name] = original_row["id"]
+        # mark every row with the similarity score to the original row
+        df.loc[cluster_mask, similarity_column_name] = original_row_similarities[df.loc[cluster_mask, "clustering_index"]]
+
     return df
 
 def cluster_data(
         df: pd.DataFrame,
-        name_similarity_threshold: float = 0.1,
-        address_similarity_threshold: float = 0.1,
+        name_duplicate_max_distance_threshold: float = 0.1,
+        address_duplicate_max_distance_threshold: float = 0.1,
         tfidf_ngram_range: tuple = (1, 1),
+        tfidf_min_df: int = 1,
+        tfidf_use_char_ngrams: bool = False,
 ) -> pd.DataFrame:
+    df.loc[:, "clustering_index"] = -1
     def cluster_group(group_df):
         try:
             # only cluster names that are defined
             name_defined_mask = (group_df["Ad-Soyad"] != "")
             group_df = cluster_by_column(
-                group_df, 
-                "Ad-Soyad", 
-                "Ad-Soyad-cluster", 
-                name_similarity_threshold, 
-                tfidf_ngram_range, 
+                df=group_df, 
+                key_column_name="Ad-Soyad", 
+                duplicate_max_distance_threshold=name_duplicate_max_distance_threshold, 
+                tfidf_ngram_range=tfidf_ngram_range, 
+                tfidf_min_df=tfidf_min_df, 
+                tfidf_use_char_ngrams=tfidf_use_char_ngrams, 
                 df_mask = name_defined_mask)
             # if name is not defined, assign -1 to cluster (means no cluster)
             group_df.loc[group_df["Ad-Soyad"] == "", "Ad-Soyad-cluster"] = -1
@@ -69,29 +117,42 @@ def cluster_data(
                 continue
             try:
                 group_df = cluster_by_column(
-                    group_df, 
-                    "merged_address", 
-                    "merged_address-cluster", 
-                    address_similarity_threshold, 
-                    tfidf_ngram_range, 
+                    df=group_df, 
+                    key_column_name="merged_address", 
+                    duplicate_max_distance_threshold=address_duplicate_max_distance_threshold, 
+                    tfidf_ngram_range=tfidf_ngram_range, 
+                    tfidf_min_df=tfidf_min_df, 
+                    tfidf_use_char_ngrams=tfidf_use_char_ngrams, 
                     df_mask=cluster_df_mask)
             except ValueError as e:
                 # in case of any errors when clustering addresses, assign -1 to every row (means no cluster)
                 group_df.loc[cluster_df_mask, 'merged_address-cluster'] = -1
         return group_df
-    return df.groupby(["İl", "İlçe", "Mahalle"], group_keys=False).apply(cluster_group)
+    df = df \
+        .groupby(["İl", "İlçe", "Mahalle"], group_keys=False) \
+        .apply(cluster_group)
+    df.drop("clustering_index", axis=1, inplace=True)
+    return df
 
 def main(
         data_path: str = "data/merged_v1_4.csv",
-        name_similarity_threshold: float = 0.1,
-        address_similarity_threshold: float = 0.1,
-        tfidf_ngram_max: int = 1,
+        name_duplicate_max_distance_threshold: float = 0.2,
+        address_duplicate_max_distance_threshold: float = 0.3,
+        tfidf_ngram_min: int = 2,
+        tfidf_ngram_max: int = 4,
+        tfidf_use_char_ngrams: bool = True,
         output_data_path: str = "data/clustered_v_1_4.csv",
         save_clustered_csv: bool = False,
     ):
     df_main = load_data(data_path)
     df_main = merge_address_columns(df_main)
-    df_main = cluster_data(df_main, name_similarity_threshold, address_similarity_threshold, (1, tfidf_ngram_max))
+    df_main = cluster_data(
+        df=df_main, 
+        name_duplicate_max_distance_threshold=name_duplicate_max_distance_threshold, 
+        address_duplicate_max_distance_threshold=address_duplicate_max_distance_threshold, 
+        tfidf_ngram_range=(tfidf_ngram_min, tfidf_ngram_max),
+        tfidf_use_char_ngrams=tfidf_use_char_ngrams,
+    )
     if save_clustered_csv:
         df_main.to_csv(output_data_path, index=False)
 
